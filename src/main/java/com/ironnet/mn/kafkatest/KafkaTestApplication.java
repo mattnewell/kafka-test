8b9dec984aa6324fd22afafad49ee89262d4e810
Matt Newell <matt.newell@ironnetcybersecurity.com>
2022-07-19 16:51:10 -0400

    sync for remote dev

--- src/main/java/com/ironnet/mn/kafkatest/KafkaTestApplication.java
+++ src/main/java/com/ironnet/mn/kafkatest/KafkaTestApplication.java
@@ -1,13 +1,54 @@
 package com.ironnet.mn.kafkatest;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class KafkaTestApplication {
 
+	private final Logger logger = LoggerFactory.getLogger(KafkaTestApplication.class);
+	private final TaskExecutor exec = new SimpleAsyncTaskExecutor();
+
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaTestApplication.class, args);
+	};
+
+	@Bean
+	@Profile("default") // Don't run from test(s)
+	public ApplicationRunner runner() {
+		return args -> {
+			System.out.println("Hit Enter to terminate...");
+			System.in.read();
+		};
+	}
+
+	@Bean
+	public KafkaAdmin admin() {
+		Map<String, Object> configs = new HashMap<>();
+		configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9999");
+		return new KafkaAdmin(configs);
+	}
+
+	@KafkaListener(id = "fooGroup", topics = "topic1")
+	public void listen(Foo2 foo) {
+		logger.info("Received: " + foo);
+		if (foo.getFoo().startsWith("fail")) {
+			throw new RuntimeException("failed");
+		}
+		this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
 	}
 
 }
